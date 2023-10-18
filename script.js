48a3ba3fd1437f03ca1979178e253cbb1892fa12
yadavhimu <106873438+yadavhimu@users.noreply.github.com>
2023-10-18 21:25:54 +0530

    index file Updated

--- script.js
+++ script.js
@@ -57,3 +57,9 @@ elems.forEach(function (elem) {
     }
   });
 });
+
+var loader = document.getElementById('Preloader');
+
+     window.addEventListener("load",function(){
+    loader.style.display = "none";
+  });
